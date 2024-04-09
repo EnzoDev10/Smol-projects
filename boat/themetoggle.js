0c45634da8fd352e6c6148522b772f9207ba73b8
Enzo Cañete <enzodev4@gmail.com>
2024-04-09 14:39:14 -0300

    Updated theme toggler and deleted some files

--- boat/themetoggle.js
+++ boat/themetoggle.js
@@ -1,26 +1,23 @@
-
 // Get all the elements that will be changed based on the theme
 let darkMode = localStorage.getItem("darkMode");
 const darkModeToggle = document.querySelector("#theme-toggle");
+const darkModeImg = document.getElementById("theme-img");
 const controlImg = document.getElementById("control-img");
 const siriImg = document.getElementById("siri-img");
-const tableBg = document.getElementById("table-bg");
 
 const logos = document.getElementsByClassName("logo");
 const nextIcons = document.getElementsByClassName("next-icon");
 const triangleIcons = document.getElementsByClassName("triangle-icon");
 const faqIcons = document.getElementsByClassName("faq-icon");
 
-
 const enableDarkMode = () => {
 	// Adds a class to the body with new values for color variables
 	document.body.classList.add("darkmode");
-    // adds classes that change icons, background and src of images
-	darkModeToggle.classList.add("dark");
+	// adds classes that change icons, background and src of images
 	menuToggle.classList.add("white-icon");
+	darkModeImg.src = "icons/dark/toggler-dark.svg";
 	controlImg.src = "images/dark/controls-dark.png";
 	siriImg.src = "images/dark/Siri-dark.svg";
-	tableBg.style.backgroundImage = "url('images/dark/table-bg-dark.svg')";
 
 	Array.from(logos).forEach((logo) => {
 		logo.src = "icons/dark/logo-dark.png";
@@ -32,12 +29,10 @@ const enableDarkMode = () => {
 
 	Array.from(triangleIcons).forEach((icon) => {
 		icon.classList.add("white-icon");
-
 	});
 
 	Array.from(faqIcons).forEach((icon) => {
 		icon.classList.add("white-icon");
-
 	});
 
 	// 2. update darkmode in the localStorage
@@ -47,11 +42,10 @@ const enableDarkMode = () => {
 const disableDarkMode = () => {
 	// on call removes the classes and reverts the src of images to the original ones
 	document.body.classList.remove("darkmode");
-	darkModeToggle.classList.remove("dark");
 	menuToggle.classList.remove("white-icon");
+	darkModeImg.src = "icons/light/toggler-light.svg";
 	controlImg.src = "images/light/controls-light.png";
 	siriImg.src = "images/light/Siri-light.svg";
-	tableBg.style.backgroundImage = "url('images/light/table-bg-light.svg')";
 
 	Array.from(logos).forEach((logo) => {
 		logo.src = "icons/light/logo-light.png";
@@ -59,25 +53,22 @@ const disableDarkMode = () => {
 
 	Array.from(nextIcons).forEach((icon) => {
 		icon.classList.remove("white-icon");
-
 	});
 
 	Array.from(triangleIcons).forEach((icon) => {
 		icon.classList.remove("white-icon");
-
 	});
 
 	Array.from(faqIcons).forEach((icon) => {
 		icon.classList.remove("white-icon");
-
 	});
 
 	// 2. update darkmode in the localStorage
 	localStorage.setItem("darkMode", null);
 };
 
-    // checks on page load if the last time the darkmode was enabled
-    // if it is, enables darkmode again
+// checks on page load if the last time the darkmode was enabled
+// if it is, enables darkmode again
 if (darkMode === "enabled") {
 	enableDarkMode();
 }
